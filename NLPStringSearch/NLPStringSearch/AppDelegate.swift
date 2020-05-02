225dd119e3329ecd7c738c322771300138d6225d
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-05-02 12:08:45 +0900

    Added Xcode Project

---
+++ NLPStringSearch/NLPStringSearch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NLPStringSearch
+//
+//  Created by Jeffrey Bergier on 2020/05/02.
+//  Copyright © 2020 Jeffrey Bergier. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
