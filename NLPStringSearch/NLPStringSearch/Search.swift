602ccbfe187281060a705009bd7b20fa2e387405
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-05-02 17:28:31 +0900

    got japanese conversion working

--- NLPStringSearch/NLPStringSearch/Search.swift
+++ NLPStringSearch/NLPStringSearch/Search.swift
@@ -6,8 +6,61 @@
 //  Copyright © 2020 Jeffrey Bergier. All rights reserved.
 //
 
+import NaturalLanguage
 import Foundation
 
 struct Search {
-    
+
+    static func japaneseToKatakana(_ input: String) -> String {
+        let input = input as NSString
+        return self.japaneseConvert(input: input, transform: kCFStringTransformLatinKatakana)
+    }
+
+    static func japaneseToLatin(_ input: String) -> String {
+        let input = input as NSString
+        return self.japaneseConvert(input: input, transform: kCFStringTransformToLatin)
+    }
+
+    private static func japaneseConvert(input: NSString, transform: CFString) -> String {
+        let range = CFRange(location: 0, length: input.length)
+        let locale = CFLocaleCreate(kCFAllocatorDefault, CFLocaleIdentifier("ja_JP" as CFString))!
+        let tokenizer = CFStringTokenizerCreate(kCFAllocatorDefault,
+                                                input as CFString,
+                                                range,
+                                                kCFStringTokenizerUnitWordBoundary,
+                                                locale)
+        var result = ""
+        var tokenType = CFStringTokenizerGoToTokenAtIndex(tokenizer, 0)
+        while tokenType.rawValue != 0 {
+            let transcribed = CFStringTokenizerCopyCurrentTokenAttribute(
+                tokenizer,
+                kCFStringTokenizerAttributeLatinTranscription
+                ) as? NSMutableString
+            if let transcribed = transcribed {
+                CFStringTransform(transcribed, nil, transform, false)
+                result += transcribed as String
+            } else {
+                let _currentRange = CFStringTokenizerGetCurrentTokenRange(tokenizer)
+                let currentRange = NSRange(location: _currentRange.location, length: _currentRange.length)
+                let originalString = input.substring(with: currentRange)
+                result += originalString
+            }
+            tokenType = CFStringTokenizerAdvanceToNextToken(tokenizer)
+        }
+        return result
+    }
+
+    // Using NL Framework
+    // Not needed because Tokenizer does two jobs in one
+    /*
+    static func words(from input: String) {
+        let tokenizer = NLTokenizer(unit: .word)
+        tokenizer.string = input
+        tokenizer.enumerateTokens(in: input.startIndex ..< input.endIndex)
+        { (range, flags) -> Bool in
+            print(input[range])
+            return true
+        }
+    }
+    */
 }
--- NLPStringSearch/NLPStringSearchTests/NLPStringSearchTests.swift
+++ NLPStringSearch/NLPStringSearchTests/NLPStringSearchTests.swift
@@ -11,24 +11,41 @@ import XCTest
 
 class NLPStringSearchTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Article Text: https://www3.nhk.or.jp/news/html/20200501/k10012415011000.html
+    let inputString = """
+    関西電力は、福井県にある大飯原子力発電所３号機の定期検査の作業開始を新型コロナウイルスの感染防止のため２か月から３か月程度、延ばすことを決めました。
+    およそ年１回の頻度で実施することが決まっている原発の定期検査では全国から作業員が集まります。
+    福井県にある大飯原発３号機でも今月８日からの検査に県外から900人前後の作業員が集まる見通しで、福井県の杉本知事は先月、新型コロナウイルスの感染防止のため、福井に来る前に作業員は２週間の自宅待機を関西電力に求めるなどしていました。
+    これに関して、大飯原発の文能一成所長が１日、地元の福井県おおい町を訪れ、定期検査の作業開始を２か月から３か月程度延ばす方針を伝えました。
+    理由は、感染の拡大を防ぐためとしています。
+    関西電力では、定期検査の開始時期については、今後、作業を請け負う協力会社などと調整したうえで決めるとしています。
+    """
+
+    let outputKatakana = """
+    カンサイデンリョクハ､フクイケンニアルオオイゲンシリョクハツデンショ3ゴウキノテイキケンサノサギョウカイシヲシンガタコロナウイルスノカンセンボウシノタメ2カゲツカラ3カゲツテイド､ノバスコトヲキメマシタ｡
+    オヨソネン1カイノヒンドデジッシスルコトガキマッテイルゲンパツノテイキケンサデハゼンコクカラサギョウインガアツマリマス｡
+    フクイケンニアルオオイゲンパツ3ゴウキデモコンゲツ8カカラノケンサニケンガイカラ900ニンゼンゴノサギョウインガアツマルミトオシデ､フクイケンノスギモトチジハセンゲツ､シンガタコロナウイルスノカンセンボウシノタメ､フクイニクルマエニサギョウインハ2シュウカンノジタクタイキヲカンサイデンリョクニモトメルナドシテイマシタ｡
+    コレニカンシテ､オオイゲンパツノブンノウイッセイショチョウガ1カ､ジモトノフクイケンオオイマチヲオトズレ､テイキケンサノサギョウカイシヲ2カゲツカラ3カゲツテイドノバスホウシンヲツタエマシタ｡
+    リユウハ､カンセンノカクダイヲフセグタメトシテイマス｡
+    カンサイデンリョクデハ､テイキケンサノカイシジキニツイテハ､コンゴ､サギョウヲウケオウキョウリョクカイシャナドトチョウセイシタウエデキメルトシテイマス｡
+    """
+    let outputLatin = """
+    kansaidenryokuha､fukuikenniaruooigenshiryokuhatsudensho3goukinoteikikensanosagyoukaishiwoshingatakoronauirusunokansenboushinotame2kagetsukara3kagetsuteido､nobasukotowokimemashita｡
+    oyosonen1kainohindodejisshisurukotogakima~tsuteirugenpatsunoteikikensadehazenkokukarasagyouingaatsumarimasu｡
+    fukuikenniaruooigenpatsu3goukidemokongetsu8kakaranokensanikengaikara900ninzengonosagyouingaatsumarumitooshide､fukuikennosugimotochijihasengetsu､shingatakoronauirusunokansenboushinotame､fukuinikurumaenisagyouinha2shuukannojitakutaikiwokansaidenryokunimotomerunadoshiteimashita｡
+    korenikanshite､ooigenpatsunobunnouisseishochouga1ka､jimotonofukuikenooimachiwootozure､teikikensanosagyoukaishiwo2kagetsukara3kagetsuteidonobasuhoushinwotsutaemashita｡
+    riyuuha､kansennokakudaiwofusegutametoshiteimasu｡
+    kansaidenryokudeha､teikikensanokaishijikinitsuiteha､kongo､sagyouwoukeoukyouryokukaishanadotochouseishitauedekimerutoshiteimasu｡
+    """
+    
+    func test_japaneseToLatin() {
+        let outputLatin = Search.japaneseToLatin(self.inputString)
+        XCTAssertEqual(outputLatin, self.outputLatin)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_japaneseToKatakana() {
+        let outputKatakana = Search.japaneseToKatakana(self.inputString)
+        XCTAssertEqual(outputKatakana, self.outputKatakana)
     }
 
 }
