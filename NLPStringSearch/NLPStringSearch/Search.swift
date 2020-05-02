ba4d8c7ce797e40993802ee7c4e32b4cc3628796
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-05-02 12:12:14 +0900

    added search file

---
+++ NLPStringSearch/NLPStringSearch/Search.swift
@@ -0,0 +1,13 @@
+//
+//  Search.swift
+//  NLPStringSearch
+//
+//  Created by Jeffrey Bergier on 2020/05/02.
+//  Copyright © 2020 Jeffrey Bergier. All rights reserved.
+//
+
+import Foundation
+
+struct Search {
+    
+}
