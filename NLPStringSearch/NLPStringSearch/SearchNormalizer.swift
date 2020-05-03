254e1f822fc7ea4aeeaa293ea167028d36728bf0
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-05-03 11:57:48 +0900

    added SearchNormalizer file

---
+++ NLPStringSearch/NLPStringSearch/SearchNormalizer.swift
@@ -0,0 +1,9 @@
+//
+//  SearchNormalizer.swift
+//  NLPStringSearch
+//
+//  Created by Jeffrey Bergier on 2020/05/03.
+//  Copyright © 2020 Jeffrey Bergier. All rights reserved.
+//
+
+import Foundation
